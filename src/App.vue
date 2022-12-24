da342d0606c00d482686899681e4494ea41d011f
Artem Melnik <melnikartem17@gmail.com>
2022-12-24 16:56:46 +0300

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Container>
+    <ChatWindow v-on:post-message="postMessage($event)">
+      <ChatMessage 
+      v-for="message in messageStore"
+      v-bind:message="message"
+      >
+      </ChatMessage>
+    </ChatWindow>
+  </Container>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/MyContainer.vue";
+import ChatWindow from "./components/ChatWindow.vue";
+import ChatMessage from "./components/ChatMessage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    Container, ChatMessage, ChatWindow,
+  },
+  data: () => ({
+    messageStore:[],
 
+  }),
+  mounted(){
+    this.getMessages()
+  },
+  methods:{
+    async getMessages(){
+      try{
+        const response = await this.axios.get('https://61bcd385d8542f0017824a2a.mockapi.io/messages')
+        this.messageStore = response.data
+      }catch (e){
+        alert(`error get ${e.data}`)
+      }
+    },
+    async postMessage(message){
+      try{
+        await this.axios.post('https://61bcd385d8542f0017824a2a.mockapi.io/messages', message )
+        this.getMessages()
+      }catch(e){
+        alert(`error post ${e.data}`)
+      }
+    }
+  }
+
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
